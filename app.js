1073f2e2b69d76e35e49d11bcef54fb6ca27a2e9
melvinDebot <melvindebot@gmail.com>
2020-03-16 18:11:50 +0100

    création de la page party

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv').config();
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+app.use(bodyParser.urlencoded({ extended : true}))
+
+app.get('/', function(req, res) {
+  res.render('index', { title: "Qui prend quoi"});
+  console.log(req.body)
+});
+
+app.post('/party', function(req, res) {
+  axios
+    .post(`${process.env.API_URL}/party`, req.body)
+    .then(({data}) => console.log(data))
+    .catch((err) => console.error(err));
+});
+
+//Page Evénement
+app.get('/party/:id', function(req, res){
+  res.render('party', {test : 'test'})
+})
+
+
+app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
+app.set('view engine', 'pug');
+
